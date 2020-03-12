381cba16a3effb6807ad7c7486c6f7310537123e
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-12 14:08:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,16 @@
 class Dog
+  
   def initialize(name)
     @name = name
-end
+  end
 
-def name 
+  def name 
   @name 
-
-end
-def name=(new_name)
+  end
+  
+  def name=(new_name)
   @name = new_name
-end
+  end
     
 end
 
